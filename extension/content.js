36e7a0a19beba6e863e15a91000608a7832e904f
matheusavi <matheusavi.47@gmail.com>
2024-10-07 01:54:51 -0300

    Add event listener to the iframe
    Fix event listener on the iframe
    
    Make the connection between the values and the tokens

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,3 @@
-alert("extension loaded");
-// Define a function to execute code on dndbeyond.com
 function handleDnDBeyond() {
   function getWarriors() {
     let warriors = document.querySelectorAll('.combatant-card');
@@ -9,7 +7,6 @@ function handleDnDBeyond() {
       hp: x.querySelector('.combatant-card__hp-current')?.innerText
     }));
 
-    // Store the result in Chrome storage
     chrome.storage.local.set({ 'dndbeyond-warriors': result }, () => {
       console.log('Warriors data saved to chrome storage');
     });
@@ -17,27 +14,22 @@ function handleDnDBeyond() {
   setInterval(getWarriors, 10000);
 }
 
-// Define a function to execute code on owlbear.rodeo
 function handleOwlbearRodeo() {
   const gameIdInput = document.querySelector('#game-id');
   if (gameIdInput) {
     const gameId = gameIdInput.value;
 
-    // Store the game ID in Chrome storage
     chrome.storage.local.set({ 'owlbear-game-id': gameId }, () => {
       console.log('Owlbear game ID saved to chrome storage');
     });
   }
 
-  // Append a list of the warriors stored on dndbeyond to a div inside the iframe using postMessage every 10 seconds
   function appendWarriors() {
-    console.log("append warriors");
     chrome.storage.local.get('dndbeyond-warriors', (data) => {
       const warriors = data['dndbeyond-warriors'];
       if (warriors && warriors.length > 0) {
-        const iframe = document.querySelector('iframe');
+        const iframe = document.querySelector("span[aria-label='Beyond health']").parentElement.parentElement.querySelector("iframe")
         if (iframe) {
-          // Send the warriors data to the iframe using postMessage
           iframe.contentWindow.postMessage({ type: 'updateCombatants', warriors: warriors }, '*');
         }
       }
@@ -48,9 +40,7 @@ function handleOwlbearRodeo() {
 }
 
 if (window.location.href.startsWith('https://www.dndbeyond.com/')) {
-  alert("handle beyond");
   handleDnDBeyond();
 } else if (window.location.href.startsWith('https://www.owlbear.rodeo/')) {
-  alert("handle rodeo");
   handleOwlbearRodeo();
 }
--- front/src/main.ts
+++ front/src/main.ts
@@ -6,6 +6,8 @@ const ID = "com.beyondhealth";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
    <input type="text" id="game-id" />  
+    <div id="selected-tokens">
+    </div>
   </div>
 `;
 
@@ -20,27 +22,109 @@ OBR.onReady(async () => {
     extensionMetadata[ID] = { identifier: getRandomString(256) };
     OBR.room.setMetadata({ ...metadata, ...extensionMetadata });
   }
+  setupContextMenu();
+  setupInitiativeList(document.getElementById("selected-tokens"));
 });
 
+function setupInitiativeList(element: any) {
+  const renderList = (items: any) => {
+    const initiativeItems = [];
+    for (const item of items) {
+      const metadata = item.metadata[`${ID}/metadata`];
+      if (metadata) {
+        initiativeItems.push({
+          name: item.name,
+        });
+      }
+    }
+
+    const nodes = [];
+    for (const initiativeItem of initiativeItems) {
+      const node = document.createElement("div");
+      const spanNode = document.createElement("span");
+      spanNode.innerText = initiativeItem.name;
+      node.appendChild(spanNode);
+
+      const selectNode = document.createElement("select");
+      selectNode.name = initiativeItem.name;
+      warriors.forEach((warrior: any) => {
+        const warriorElement = document.createElement("option");
+        warriorElement.text = warrior.name;
+        warriorElement.value = warrior.hp;
+        selectNode.appendChild(warriorElement);
+      });
+      node.appendChild(selectNode);
+
+      nodes.push(node);
+    }
+    element.replaceChildren(...nodes);
+  };
+  OBR.scene.items.onChange(renderList);
+}
+
+function setupContextMenu() {
+  OBR.contextMenu.create({
+    id: `${ID}/context-menu`,
+    icons: [
+      {
+        icon: "/icon.svg",
+        label: "Track life",
+        filter: {
+          every: [
+            { key: "layer", value: "CHARACTER" },
+            { key: ["metadata", `${ID}/metadata`], value: undefined },
+          ],
+        },
+      },
+      {
+        icon: "/remove.svg",
+        label: "Stop tracking life",
+        filter: {
+          every: [{ key: "layer", value: "CHARACTER" }],
+        },
+      },
+    ],
+    onClick(context) {
+      const addToInitiative = context.items.every(
+        (item) => item.metadata[`${ID}/metadata`] === undefined,
+      );
+      if (addToInitiative) {
+        OBR.scene.items.updateItems(context.items, (items) => {
+          for (let item of items) {
+            console.log(JSON.stringify(item));
+            item.metadata[`${ID}/metadata`] = {
+              name: item.name, //here insert something to tick as tracking
+              // initiative,
+            };
+          }
+        });
+      } else {
+        OBR.scene.items.updateItems(context.items, (items) => {
+          for (let item of items) {
+            delete item.metadata[`${ID}/metadata`];
+          }
+        });
+      }
+    },
+  });
+}
+
 async function updateLife() {
-  const metadata = await OBR.room.getMetadata();
-  console.log(metadata);
-  const resolved = await fetchCharacterData();
+  const selector = document.querySelector("select");
   await OBR.scene.items.updateItems(
-    (x) => x.name.startsWith("mao do bobao") && x.layer == "CHARACTER",
+    (x) => x.name == selector?.name && x.layer == "CHARACTER",
     (list) => {
       for (var item of list) {
-        item.text.plainText = "mao do bobao " + resolved;
+        item.text.plainText =
+          selector?.selectedOptions[0].text +
+          " - " +
+          selector?.selectedOptions[0].value;
       }
     },
   );
 }
 
 setInterval(updateLife, 10000);
-
-async function fetchCharacterData() {
-  return "Bla";
-}
 
 function getRandomString(length: number): string {
   const charset =
@@ -57,3 +141,11 @@ function getRandomString(length: number): string {
 
   return result;
 }
+
+let warriors: any[] = [];
+
+window.addEventListener("message", function (event) {
+  if (event.data.type === "updateCombatants") {
+    warriors = event.data.warriors;
+  }
+});
